7e4063876556de4810204827fbe3bf9f657e8353
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-05-06 12:32:36 +0100

    added test.js to the course

---
+++ examples/nathanusiverity/test.js
@@ -0,0 +1,14 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('scheme.pegjs', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+// Do a test
+r = parse("(a b c)");
+console.log(r);
+assert.deepEqual( r, ["a", "b", "c"] );
