bb44e1de645140bece7dda5df1a66c2e4f55c30c
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-04-24 21:37:00 +0100

    mainnotpredicate.js

--- examples/mainnotpredicate.js
+++ examples/mainnotpredicate.js
@@ -1,8 +1,10 @@
 var PEG = require("./notpredicate.js");
-var input = process.argv[2] || "// one comment\n"+
-                               "// another comment \t/\n"+
-                               "/* a\n"+
-                               "   third comment */";
+var input = process.argv[2] || `
+// one comment
+// another comment 
+/* a
+   third comment */
+`;
 console.log("\n*****\n"+input+"\n*****\n");
 var r = PEG.parse(input);
 console.log(r);
