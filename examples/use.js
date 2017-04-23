1a16e51d04ba373020718afec024f44d3acf2882
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-04-23 22:07:41 +0100

    use.js

--- examples/use.js
+++ examples/use.js
@@ -1,38 +1,12 @@
-#!/usr/local/bin/node
-var rep = '/Users/casiano/Dropbox/src/javascript/PLgrado/pegjs/';
-var fs = require('fs');
-
+#!/usr/local/bin/node --harmony_destructuring
 
 var arguments = process.argv.splice(2);
 //console.log(arguments);
 var module = arguments[0];
 var input  = arguments[1];
-module = "./"+module;
 
-/* compile grammar */
-var FFI = require("node-ffi");
-var libc = new FFI.Library(null, {
-  "system": ["int32", ["string"]]
-});
-var run = libc.system;
+var parse = require(module).parse;
+console.log(`Processing <${input}>`);
+var r = parse(input);
+console.log(r);
 
-function compile() {
-    var PEG = require(module+'.js');
-    //console.log(PEG);
-    if (input) {
-      input = input.toString().trim();
-      console.log("input = "+input);
-      var r = PEG.parse(input);
-      console.log("result = "+JSON.stringify(r));
-    }
-}
-
-fs.exists(module+".js", function(exists) {
-  if (exists) {
-    compile();
-  } else {
-    console.log("Can't find "+module+".js Compiling "+module+'.pegjs');
-    run(rep+"bin/pegjs "+module+'.pegjs');
-    compile();
-  }
-});
