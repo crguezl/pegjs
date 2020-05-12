4f7a07fc691a4ca1744b14d75982afea62cf7520
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-05-12 09:19:13 +0100

    initializer.js

--- examples/initializer.js
+++ examples/initializer.js
@@ -1,20 +1,24 @@
 "use strict";
 const PEG = require("pegjs");
 const grammar = `
- {                             
-   const util = require("util");     
-   const g = "visible variable"; 
-   console.log("Inside Initializer! options = "+util.inspect(options)); 
- }                             
- start = 'a' { console.log('g = '+g); return 1; } 
-       /  &  { console.log('inside predicate: g = '+g); return true; } 'b' { return 2; }
+ {
+   const ins = require("util").inspect;
+   const g = "visible variable";
+   console.log("Inside Initializer! options = "+ins(options));
+ }
+ start = 'a' { console.log('g = '+g); return 1; }
+       /  &  { console.log('inside predicate: g = '+ins(g)+' options = '+ins(options)); 
+               return true; 
+             } 
+         'b' { return 2; }
 `;
 
 const parser = PEG.generate(grammar);
 console.log("PARSING 'a'");
 let r = parser.parse("a", { x: 'hello' });
-console.log(r); 
+console.log(r);
+
 console.log("PARSING 'b'");
 r = parser.parse("b");
-console.log(r); 
+console.log(r);
 
